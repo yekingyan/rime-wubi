156abe70954cf6153f6718caf4f92f029cd2e884
yekingyan <529616@gmail.com>
2018-10-20 15:15:23 +0800

    防止会删除标点

--- wubi86_single.py
+++ wubi86_single.py
@@ -1,5 +1,6 @@
 import re
 import time
+import os
 
 
 def select_single(line):
@@ -8,7 +9,7 @@ def select_single(line):
     :return: 非词组开头的行
     """
     # 返回ascii码开头的行数，即非汉字开头
-    if ord(line[0]) <= 127:
+    if ord(line[0]) <= 127 or 'zz' in line:
         return line
     # 返回有内容，且第二个字符为空格的行数
     if len(line) >= 2:
@@ -19,22 +20,25 @@ def select_single(line):
     return None
 
 
-def main():
+def main(origin, new_file):
     """
     读取文件，写入文件
     """
-    with open('wubi86.dict.yaml', 'r', encoding='utf-8') as f:
+    with open(origin, 'r', encoding='utf-8') as f:
         for line in f:
             s = select_single(line)
             if s is not None:
                 print(s)
-                with open('wubi86_single.dict.yaml', 'a', encoding='utf-8') as file:
+                with open(new_file, 'a', encoding='utf-8') as file:
                     file.write(s)
 
 
 if __name__ == '__main__':
-    # print(len('	'))
+    path = 'wubi86_single.dict.yaml'
+    if os.path.exists(path):
+        os.remove(path)
+
     start = time.time()
-    main()
+    main('wubi86.dict.yaml', path)
     end = time.time()
-    print(f'运行时间:{end-start}秒')
+    print(f'运行时间:{int(end-start)}秒')
