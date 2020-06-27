a8e21e49098b8d0db524b4563d5c2e57919e7dc0
yekingyan <529616@gmail.com>
2020-06-27 13:14:17 +0800

    保留z万能键字库、脚本调整

--- wubi86_single.py
+++ wubi86_single.py
@@ -1,44 +1,37 @@
 import re
-import time
-import os
 
 
-def select_single(line):
-    """
-    :param line:一行字符串内容
-    :return: 非词组开头的行
-    """
-    # 返回ascii码开头的行数，即非汉字开头
-    if ord(line[0]) <= 127 or 'zz' in line:
-        return line
-    # 返回有内容，且第二个字符为空格的行数
-    if len(line) >= 2:
-        if re.search(r'\S', line[1]) is not None:
-            pass
-        else:
-            return line
-    return None
+INPUT_FILE_PATH = "wubi86.dict.yaml.backup"
+OUTPUT_FILE_PATH = "wubi86.dict.yaml"
+CUT_WORD = "工	a	99454797	aa"
 
 
-def main(origin, new_file):
-    """
-    读取文件，写入文件
-    """
-    with open(origin, 'r', encoding='utf-8') as f:
+def get_trim_line():
+    stay_line = []
+    with open(INPUT_FILE_PATH, encoding="utf8") as f:
+        trim_working = False
         for line in f:
-            s = select_single(line)
-            if s is not None:
-                print(s)
-                with open(new_file, 'a', encoding='utf-8') as file:
-                    file.write(s)
+            if re.match(CUT_WORD, line):
+                trim_working = True
+            if not trim_working:
+                stay_line.append(line)
+            else:
+                line_list = line.split("	")
+                word = line_list[0]
+                code = line_list[1]
+                if word.startswith("#") or len(word) == 1 or code.startswith("z"):
+                    stay_line.append(line)
+
+        return stay_line
+
+
+def main():
+    stay_line = get_trim_line()
+    with open(OUTPUT_FILE_PATH, "w+", encoding="utf8") as f:
+        for line in stay_line:
+            f.writelines(line)
 
 
 if __name__ == '__main__':
-    path = 'wubi86_single.dict.yaml'
-    if os.path.exists(path):
-        os.remove(path)
-
-    start = time.time()
-    main('wubi86.dict.yaml', path)
-    end = time.time()
-    print(f'运行时间:{int(end-start)}秒')
+    main()
+    # get_trim_line()
